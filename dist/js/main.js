d7a3ecae3699978ed120f845152664cf17836957
Md. Hamidur Rhaman <hamidpioneer@yahoo.com>
2018-09-07 02:35:28 +0600

    Project Done

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $('.owl-carousel.media-section__slider, .owl-carousel.custom-section__slider, .owl-carousel.related-section__slider').owlCarousel({
+        items: 1,
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        dots: true,
+    });
+});
