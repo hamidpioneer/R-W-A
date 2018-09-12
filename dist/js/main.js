ea76bdc50d4d9f6a66df534a276b91317337cdfb
Md. Hamidur Rhaman <hamidpioneer@yahoo.com>
2018-09-12 14:31:26 +0600

    Slider NEXT/PREV item design SOLVED (partially)

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,11 +3,11 @@ $( document ).ready(function() {
 
     $('.owl-carousel.media-section__slider, .owl-carousel.custom-section__slider, .owl-carousel.related-section__slider').owlCarousel({
         items: 1,
-        loop:true,
-        margin:10,
-        nav:true,
-        navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
         dots: true,
         autoHeight: true,
+        // margin:10,
+        // loop:true,
+        // nav:true,
+        // navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
     });
 });
